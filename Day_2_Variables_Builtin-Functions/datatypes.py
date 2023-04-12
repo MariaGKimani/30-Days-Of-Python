0b69cc3919989bc709356a973b354a137964769d
Mariagoretti <mariakimanigoreti@gmail.com>
2023-04-12 19:03:59 +0300

    Variables and functions

---
+++ Day_2_Variables_Builtin-Functions/datatypes.py
@@ -0,0 +1,69 @@
+#Day 2 [2]: 30 Days of python programming'
+
+first_name ="Mariagoretti";
+last_name = "Kimani";
+full_name = "Mariagoretti Kimani";
+country = "Kenya"
+city = "Kigali"
+age =22
+year = 2000
+is_married = False
+is_true = True
+is_light_on = False
+
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+
+
+print(len(first_name))
+print(first_name ==last_name)
+
+num_one = 5
+num_two = 4
+total = num_one + num_two
+print(total)
+
+diff = num_two -num_one
+print(diff)
+product = num_two * num_one
+
+division =num_two /num_one
+print(division)
+
+remainder =num_two % num_one
+print(remainder)
+
+exp =num_one ** num_two
+print(exp)
+
+floor_division = num_one // num_two
+print(floor_division)
+
+
+
+area_of_circle = 3.14 * 30 *30
+print(area_of_circle)
+
+circum_of_circle = 3.14 *60
+print(circum_of_circle)
+
+
+first,last,full,countries,cities,age,is_marry = 'Deo','Massawe','Deo Massawe','Rwanda','Nairobi',35,True
+
+
+# c = (input("Enter radius of a circle"))
+# print(c.area())
+
+
+
+
+
+
---
+++ Day_2_Variables_Builtin-Functions/variables.py
@@ -0,0 +1,20 @@
+#Day 2[1]: 30 Days of python programming'
+
+first_name ="Mariagoretti";
+last_name = "Kimani";
+full_name = "Mariagoretti Kimani";
+country = "Kenya"
+city = "Kigali"
+age =22
+year = 2000
+is_married = False
+is_true = True
+is_light_on = False
+
+first,last,full,countries,cities,age,is_marry = 'Deo','Massawe','Deo Massawe','Rwanda','Nairobi',35,True
+print(first, last, countries, age, is_marry)
+print('First name:', first_name)
+print('Last name: ', last_name)
+print('Country: ', country)
+print('Age: ', age)
+print('Married: ', is_married)
