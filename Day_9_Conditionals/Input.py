28ccaa66b794de7ba3d7c2032a317566e0b65179
Mariagoretti <mariakimanigoreti@gmail.com>
2023-04-25 20:43:48 +0300

    conditionals

---
+++ Day_9_Conditionals/Input.py
@@ -0,0 +1,44 @@
+# Get user input using input(“Enter your age: ”). 
+# If user is 18 or older, give feedback: 
+# You are old enough to drive. If below 18 give feedback to wait for 
+# the missing amount of years
+age =int(input("Enter your age: "))
+if age >= 18:
+    print ("You're old enough to drive")
+else:
+    missing_years = 18-age
+    print("You need to wait", missing_years, "more year(s) before you can drive.")
+
+
+# Compare the values of my_age
+# and your_age using if … else. 
+# Who is older (me or you)? 
+# Use input(“Enter your age: ”) to get the age as input.
+#  You can use a nested condition to print 'year' for 1 year difference in age, 
+#  'years' for bigger differences, and a custom text if my_age = your_age. 
+
+my_age =22
+your_age = int(input("Enter your age: "));
+difference = your_age -my_age
+if difference == -1:
+    print("year")
+else:
+    my_age = your_age
+    print("good")
+
+
+# Get two numbers from the user using input prompt. 
+# If a is greater than b return a is greater than b, 
+# if a is less b return a is smaller than b, else a is equal to b.
+def numbers():
+    a = int(input("Enter a number:"))
+    b = int(input("Enter a number:"))
+    if a > b:
+        return "a is greater than b"
+    elif a < b:
+        return "a is smaller than b"
+    else:
+        return "a is equal to b"
+    
+print(numbers())
+                                                                                               
---
+++ Day_9_Conditionals/input2.py
@@ -0,0 +1,61 @@
+# Write a code which gives grade to students according to theirs scores:
+
+# 80-100, A
+# 70-89, B
+# 60-69, C
+# 50-59, D
+# 0-49, F
+
+def score (grade):
+    if grade >= 80 and grade <= 100:
+        print("A")
+    elif grade >= 70 and grade <= 89:
+        print("B")
+    elif grade >= 60 and grade <= 69:
+        print("C")
+    elif grade >= 50 and grade <= 59:
+        print("D")
+    elif grade >= 0 and grade <= 49:
+        print("F")
+grade1 = score(85)
+
+# Check if the season is Autumn, Winter, Spring or Summer.
+#  If the user input is: September, October or November, 
+# the season is Autumn. December, January or February, 
+# the season is Winter. March, April or May, 
+# the season is Spring June, July or August, the season is Summer
+
+x = "September","October","November"
+y = "December","January","February"
+z = "March","April","May"
+w = "June","July","August"
+person = input("Enter month: ")
+if person in x :
+    print("Autumn")
+elif person in y:
+    print("Winter")
+elif person in z:
+    print("spring")
+elif person in w:
+    print("summer")
+
+
+
+         
+
+
+
+
+
+
+
+
+
+
+# fruits = ['banana', 'orange', 'mango', 'lemon']
+# fruit = input("Name a fruit: ")
+# if fruit  in fruits:
+#     print("that fruit already exist")
+# else:
+#     fruits.append(fruit)
+#     print(fruits)
